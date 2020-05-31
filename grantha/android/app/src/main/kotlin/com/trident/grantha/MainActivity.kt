ccd317c67d9765949e77dd084c24d0d294d99262
rushannanayakkara <rushannana@gmail.com>
2020-06-01 00:41:07 +0530

    android splash screen done

---
+++ grantha/android/app/src/main/kotlin/com/trident/grantha/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.trident.grantha
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ grantha/lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'RouteGenerator.dart';
+
+void main() => runApp(new MyApp());
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  void initState() {
+    super.initState();
+    Future.delayed(
+      Duration(seconds: 3),
+      () {
+        Navigator.pushNamed(context, '/');
+      },
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      title: 'Grantha',
+      theme: new ThemeData(
+        primarySwatch: Colors.grey,
+      ),
+      initialRoute: '/',
+      onGenerateRoute: RouteGenerator.generateRoute,
+    );
+  }
+}
